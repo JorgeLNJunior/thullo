ba21835136823e13177c0c1cb3077c3dc1e1a558
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-09-05 16:55:23 -0300

    style: organizar o imports do main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
 import './index.css'
+
 import App from './App.vue'
+
+import { createApp } from 'vue'
 import { router } from './routes/router'
 
 createApp(App).use(router).mount('#app')
